b23d113e391d47c81e6d7728cb5a4c498d65f453
Preston <pmar116@uw.edu>
2020-02-20 21:26:38 -0800

    add favicon update html files

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -17,8 +17,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
-
-SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
+secret = open("secrets.txt","r")
+SECRET_KEY = secret.read().rstrip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
