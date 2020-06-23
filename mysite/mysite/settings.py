ebf42d4bc235c63529ff6fe68b111f9c3eb7bb4f
Preston <pmar116@uw.edu>
2020-06-22 19:59:55 -0700

    fix database type for prod

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -73,24 +73,25 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+if DEBUG:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
     }
-}
-"""
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'cookbook',
-        'USER': 'cookbookadmin',
-        'PASSWORD': passwords['db_password'],
-        'HOST': 'localhost',
-        'PORT': '',
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'cookbook',
+            'USER': 'cookbookadmin',
+            'PASSWORD': passwords['db_password'],
+            'HOST': 'localhost',
+            'PORT': '',
+        }
     }
-}
-"""
+
 
 
 # Password validation
